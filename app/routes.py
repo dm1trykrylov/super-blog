965e742236302344a1adb2a406b2584fce7a75c3
dm1trykrylov <231014d@gmail.com>
2022-11-27 16:41:40 +0300

    Add config.py for secret keys

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,16 @@ from app.forms import LoginForm
 @app.route('/index')
 def index():
     user = {'username': 'Dmitry'} # mock user
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
     return render_template('index.html', title='Home', user=user)
 
 @app.route('/login')
