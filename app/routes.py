766f9326f023f80e590fdb72b6e6ceb7f99c49e3
dm1trykrylov <231014d@gmail.com>
2022-11-27 18:09:56 +0300

    Handle registration

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user
 from app.models import User
 
@@ -19,7 +19,22 @@ def index():
             'body': 'Hi'
         }
     ]
-    return render_template('index.html', title='Home', posts = posts)
+    return render_template('index.html', title='Home', posts=posts)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -35,6 +50,7 @@ def login():
         login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
 
 @app.route('/logout')
 def logout():
