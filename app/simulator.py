23ea8a32f2933e93fb04134e74b0ed1d18b3f3ae
dm1trykrylov <231014d@gmail.com>
2023-06-21 00:05:15 +0400

    Chang use of sympy

--- app/simulator.py
+++ app/simulator.py
@@ -1,7 +1,7 @@
 # import necessary packages
 # import pandas as pd
 # import numpy as np
-import sympy as sp
+from sympy import *
 #from sympy.plotting import plot
 import matplotlib.pyplot as plt
 
@@ -19,18 +19,18 @@ def differentiate(X, Y):
 
 def simulate(l_0, l_1, l_2, l_3, filename1, filename2):
   # angle
-  phi_1 = sp.symbols('\phi_1')
+  phi_1 = symbols('\phi_1')
   # links
-  l0, l1, l2, l3 = sp.symbols('l_0, l_1, l_2, l_3')
+  l0, l1, l2, l3 = symbols('l_0, l_1, l_2, l_3')
   # relative lengths
   r2, r3, r4 = l2 / l1, l3 / l1, l0 / l1
 
   # Equations of the angular coordinates of the links
-  phi_b = sp.sign(-sp.sin(phi_1)) * sp.acos((r4 - sp.cos(phi_1)) / (sp.sqrt(1 + r4 ** 2 - 2 * r4 * sp.cos(phi_1))))
+  phi_b = sign(-sin(phi_1)) * acos((r4 - cos(phi_1)) / ( sqrt(1 + r4 ** 2 - 2 * r4 *  cos(phi_1))))
 
-  phi_3 = phi_b + sp.acos((r2 ** 2 - r3 ** 2 - r4 ** 2 + 2 * r4 * sp.cos(phi_1) - 1) / (2 * r3 * sp.sqrt(1 + r4 ** 2 - 2 * r4 * sp.cos(phi_1))))
+  phi_3 = phi_b +  acos((r2 ** 2 - r3 ** 2 - r4 ** 2 + 2 * r4 *  cos(phi_1) - 1) / (2 * r3 *  sqrt(1 + r4 ** 2 - 2 * r4 *  cos(phi_1))))
 
-  phi_2 = phi_b + sp.acos((1 + r2 ** 2 - r3 ** 2 + r4 ** 2 - 2 * r4 * sp.cos(phi_1)) / (2 * r2 * sp.sqrt(1 + r4 ** 2 - 2 * r4 * sp.cos(phi_1))))
+  phi_2 = phi_b +  acos((1 + r2 ** 2 - r3 ** 2 + r4 ** 2 - 2 * r4 *  cos(phi_1)) / (2 * r2 *  sqrt(1 + r4 ** 2 - 2 * r4 *  cos(phi_1))))
 
   # read link lengths
   # link_table = pd.read_csv(data_filename)
@@ -40,8 +40,8 @@ def simulate(l_0, l_1, l_2, l_3, filename1, filename2):
   phi_2 = phi_2.subs([(l0, l_0), (l1, l_1), (l2, l_2), (l3, l_3)]) 
   phi_3 = phi_3.subs([(l0, l_0), (l1, l_1), (l2, l_2), (l3, l_3)])
   # convert degrees to radians
-  phi_2 = phi_2.subs(phi_1, phi_1 / 180 * sp.pi)
-  phi_3 = phi_3.subs(phi_1, phi_1 / 180 * sp.pi)
+  phi_2 = phi_2.subs(phi_1, phi_1 / 180 *  pi)
+  phi_3 = phi_3.subs(phi_1, phi_1 / 180 *  pi)
 
   # generate points to plot
   pts_step = 360 / points_count
@@ -49,8 +49,8 @@ def simulate(l_0, l_1, l_2, l_3, filename1, filename2):
   Y2 = [phi_2.subs(phi_1, x) for x in X]
   Y3 = [phi_3.subs(phi_1, x) for x in X]
   # convert to degrees
-  Y2 = [y / sp.pi * 180 for y in Y2]
-  Y3 = [y / sp.pi * 180 + 180 for y in Y3]
+  Y2 = [y /  pi * 180 for y in Y2]
+  Y3 = [y /  pi * 180 + 180 for y in Y3]
 
   # plot angles
   fg = plt.axes()
